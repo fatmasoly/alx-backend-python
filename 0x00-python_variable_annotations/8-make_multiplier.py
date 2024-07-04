cc466ce4be2fdc35702fad4450dab8732d859e4a
fatma soliman <fatmacivil@hotmail.com>
2024-07-04 14:02:55 +0300

    multi

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Type-annotated function make_multiplier that takes a float multiplier as"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Type-annotated function make_multiplier that returns a function """
+    def multi_float(n: float) -> float:
+        """Type-annotated function make_multiplier that takes a float multiplier as"""
+        return n * multiplier
+    return multi_float
