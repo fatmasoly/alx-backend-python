03828cc0e7db4033e8557729db6e49bd65241586
fatma soliman <fatmacivil@hotmail.com>
2024-07-04 14:17:01 +0300

    fix kv

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Type-annotated function to_kv that takes a string k and an int OR float v"""
 
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> float:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return a tuple containing k and the square of v."""
     return (k, v ** 2)
