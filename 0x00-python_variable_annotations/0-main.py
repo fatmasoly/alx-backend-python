3e2eadc1a726500c077ad522f665200822a67f06
fatma soliman <fatmacivil@hotmail.com>
2024-07-04 12:52:44 +0300

    2nd

--- 0x00-python_variable_annotations/0-main.py
+++ 0x00-python_variable_annotations/0-main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-add = __import__('0-add').add
 
-print(add(1.11, 2.22) == 1.11 + 2.22)
-print(add.__annotations__)
+
+concat = __import__('1-concat').concat
+
+str1 = "egg"
+str2 = "shell"
+
+print(concat(str1, str2) == "{}{}".format(str1, str2))
+print(concat.__annotations__)
