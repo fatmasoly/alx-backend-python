1ef17547903d166438d66b6d14549890d18a2012
fatma soliman <fatmacivil@hotmail.com>
2024-07-08 16:58:22 +0300

    task4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """[summary]"""
+    delays = await asyncio.gather(*(task_wait_random(max_delay)
+                                    for _ in range(n)))
+    sorted_delays = []
+    while delays:
+        min_delay = min(delays)
+        sorted_delays.append(min_delay)
+        delays.remove(min_delay)
+
+    return sorted_delays
