4e3d54494f22a2a0d446f88e31ad4d66982e02fb
fatma soliman <fatmacivil@hotmail.com>
2024-07-08 17:30:02 +0300

    fx

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Run task_wait_random n times with the specified max_delay."""
+    """Asynchronously waits for random delays and returns a sorted list of"""
     delays = await asyncio.gather(
         *(task_wait_random(max_delay) for _ in range(n))
     )
@@ -18,24 +18,3 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
         delays.remove(min_delay)
 
     return sorted_delays
-
-# #!/usr/bin/env python3
-
-# import asyncio
-# from typing import List
-
-# task_wait_random = __import__('3-tasks').task_wait_random
-
-
-# async def task_wait_n(n: int, max_delay: int) -> List[float]:
-#     """Asynchronously waits for random delays and returns a sorted list of"""
-#     delays = await asyncio.gather(
-#         *(task_wait_random(max_delay) for _ in range(n))
-#     )
-#     sorted_delays = []
-#     while delays:
-#         min_delay = min(delays)
-#         sorted_delays.append(min_delay)
-#         delays.remove(min_delay)
-
-#     return sorted_delays
